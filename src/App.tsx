041e60fc2d7a742be0fa1ba7c1e385fdf6204f74
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-05-03 23:31:43 -0700

    add Projects file and update App file

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Bio from './components/Bio';
+import Projects from './components/Projects';
 import EmailMe from './components/EmailMe';
 import './App.css';
 
@@ -14,6 +15,7 @@ const App: React.FC = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/bio" element={<Bio />} />
+                    <Route path="/projects" element={<Projects />} />
                     <Route path="/emailme" element={<EmailMe />} />
                 </Routes>
             </div>
---
+++ src/components/Projects.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Projects: React.FC = () => {
+    return (
+        <h2>My Projects</h2>
+    );
+};
+
+export default Projects;
