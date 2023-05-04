85de313369f998cea59afc63de109b41a8095e28
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-05-03 23:27:59 -0700

    update app and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Bio from './components/Bio';
 import EmailMe from './components/EmailMe';
 import './App.css';
 
@@ -12,6 +13,7 @@ const App: React.FC = () => {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/bio" element={<Bio />} />
                     <Route path="/emailme" element={<EmailMe />} />
                 </Routes>
             </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,9 @@ const Navbar: React.FC = () => {
         <nav>
             <ul>
                 <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
                     <Link to="/bio">Bio</Link>
                 </li>
                 <li>
