e6ed2536bb4378b92a34060a5ef7f23db957e9c9
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-05-03 23:09:54 -0700

    remove logo file and update App file

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import EmailMe from './components/EmailMe';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
