0080bbf14124685a86366efff7b4ed2151b5be5a
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-05-04 12:16:21 -0700

    add Box component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Box } from '../components/Box';
 
 const Home: React.FC = () => {
     return (
-        <h2>Home</h2>
+        <div>
+            <h2>Home</h2>
+            <Canvas>
+                <ambientLight intensity={0.5} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                <pointLight position={[-10, -10, -10]} />
+                <Box position={[0, 0, 0]} />
+            </Canvas>
+        </div>
     );
 };
 
