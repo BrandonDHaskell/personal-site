1b0bcb4ad4973acbb1b654152faf9b702d1af2bc
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-05-04 10:07:56 -0700

    update bio and manifest with image url

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Bio: React.FC = () => {
+    console.log(process.env.PUBLIC_URL);
     return (
-        <h2>My Bio</h2>
+        <div>
+            <h2>My Bio</h2>
+            <img src={`images/profile_pic_2_medium.jpg`} alt="Profile pic" />
+        </div>
     );
 };
 
