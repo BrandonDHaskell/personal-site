089e83b9766e7522fca4058312171c603dad4e62
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-05-11 14:06:27 -0700

    add projects and project card

--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -11,13 +11,13 @@ const ExperienceItem: React.FC<ExperienceItemProps> = ({ jobTitle, startStop, de
     return (
         <div className="experience-item">
             <div className="timeline-card">
-                <div className="job-info flex items-center justify-left px-10">
-                    <div className=" text-lg mb-2 font-bold md:text-lg lg:text-xl dark:text-sky-500 pr-4">{jobTitle}</div>
-                    <div className="start-stop mb-2 text-lg md:text-lg lg:text-xl italic dark:text-sky-500">{startStop}</div>
+                <div className="job-info">
+                    <div className="job-title">{jobTitle}</div>
+                    <div className="start-stop">{startStop}</div>
                 </div>
                 <div className="job-desc pb-6">
                     {description.map((desc, index) => (
-                        <p key={index} className="text-left text-sky-600 px-10 py-2">{desc}</p>
+                        <p key={index} className="job-desc">{desc}</p>
                     ))}
                 </div>
             </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,6 +4,28 @@ const Projects: React.FC = () => {
     return (
         <div className="projects">
             <h2 className="mb-8 mt-8 text-4xl font-bold md:text-5xl lg:text-6xl dark:text-sky-500">Projects</h2>
+            <div className="project-list flex justify-center m-9">
+                <div className="project-card rounded bg-rose-900/75 p-8 max-w-xs ring-2 ring-offset-2 ring-stone-900 shadow-2xl shadow-gray-400/75 hover:shadow-sky-500">
+                    <a href="https://BrandonDHaskell.github.io/pswd-generator/">
+                        <div className="project-info rounded">
+                            <h3 className="project-title job-title">Password Generator</h3>
+                            <p className="job-desc text-sm">Generate a random password with to meet your criteria</p>
+                            <img className="rounded border-2 border-stone-900" src="images/pswd-generator.png" alt="password generator"></img>
+                            <div className="flex justify-center">
+                                <div>
+                                    <span className="box-decoration-clone bg-gradient-to-r from-yellow-600 to-sky-200 text-sky-700 px-2 mx-2 rounded-lg">JavaScript</span>
+                                </div>
+                                <div><span className="box-decoration-clone bg-gradient-to-r from-yellow-600 to-sky-200 text-sky-700 px-2 mx-2 rounded-lg">HTML5</span></div>
+                                <div><span className="box-decoration-clone bg-gradient-to-r from-yellow-600 to-sky-200 text-sky-700 px-2 mx-2 rounded-lg">CSS</span></div>
+                            </div>
+                        </div>
+                    </a>
+                    <div className="flex justify-center text-sky-400">
+                        <a href="https://github.com/BrandonDHaskell/pswd-generator">Source code</a>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
