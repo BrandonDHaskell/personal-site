3bd1cd4cd36a1d62e500b117478b4197e4cbcfba
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-05-10 21:52:00 -0700

    update braces in Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar: React.FC = () => {
     return (
         <nav className="flex dark:text-sky-400 mt-2">
-            <span className="xs:text-xl sm:text-2xl md:text-3xl text-amber-200">{'{'}</span>
+            <span className="xs:text-xl sm:text-2xl md:text-3xl text-yellow-600">{'{'}</span>
             <ul className="flex">
                 <li className="flex-auto mx-4 pt-1 xs:text-md sm:text-lg md:text-xl">
                     <Link to="/">Home</Link>
@@ -19,7 +19,7 @@ const Navbar: React.FC = () => {
                     <Link to="/emailme">Contact</Link>
                 </li>
             </ul>
-            <span className="xs:text-xl sm:text-2xl md:text-3xl text-amber-200 pr-4">{'}'}</span>
+            <span className="xs:text-xl sm:text-2xl md:text-3xl text-yellow-600 pr-4">{'}'}</span>
         </nav>
     );
 };
