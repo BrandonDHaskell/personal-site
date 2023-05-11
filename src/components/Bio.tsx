eec6fe00a0b1e5fae4070cda5f3706727a460b14
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-05-10 20:55:20 -0700

    add Experience to Bio page

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -17,6 +17,18 @@ const Bio: React.FC = () => {
                     During my ten years at Amazon, I embraced roles with increasing responsibility such as Data Analyst, Systems Administrator, and as a Technical Program Manager for Amazon Care. In these positions, I closely collaborated with engineering, change program management, compliance, and operations teams. My technical training includes Full Stack development using the MERN stack (MongoDB, Express, React, Node) and programming experience in JavaScript, HTML, CSS, Java, Python, C++, C, SQL, VBA, and TypeScript. I specialize in Web Development, Technical Program Management, and Systems Administration.
                 </p>
             </div>
+            
+            <h2 className="mb-8 mt-8 text-4xl font-bold md:text-5xl lg:text-6xl dark:text-sky-500">Experience</h2>
+            <div className="timeline-card">
+                <div className="job-info flex items-center justify-left px-10">
+                    <div className=" text-lg mb-2 font-bold md:text-lg lg:text-xl dark:text-sky-500 pr-4">Technical Program Manager - Amazon Care</div>
+                    <div className="start-stop mb-2 text-lg md:text-lg lg:text-xl italic dark:text-sky-500">Jul 2020 - May 2021</div>
+                </div>
+                <div className="job-desc">
+                    <p className="text-left text-sky-500 px-10">In this role I owned product security workstreams for Amazon's brand new telehealth service Amazon Care, ahead of its public launch in May 2021. As a TPM, my role was to bring together engineering, product, operations, legal, and compliance teams to accelerate integration of 3rd party services {'('}e.g., medical staffing, records companies{')'} to ensure data security including HIPAA compliance on the Amazon Care platform.</p>
+                </div>
+            </div>
+
         </section>
     );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 const Header: React.FC = () => {
     return (
         <header className="container">
-            <div className="flex flex-nowrap gap-4 columns-3">
+            <div className="flex flex-nowrap columns-3">
                 <div className="flex">
                     <span className="float-left text-xs text-sky-600 py-3 pl-2 pr-1">const</span>
                     <div className="relative w-80 sm:w-70 py-4">
@@ -12,7 +12,7 @@ const Header: React.FC = () => {
                             <img src="/images/BrandonDHaskell.png" alt="Brandon D Haskell"></img>
                         </a>
                     </div>
-                    <span className="text-2xl text-white pl-6 pt-3">=</span>
+                    <span className="text-2xl text-white sm:px-2 md:px-6 pt-3">=</span>
                 </div>
                 <Navbar />
             </div>
